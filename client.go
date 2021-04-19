49abda1504896bfdac0e67297327bb0d7c024615
em_win <1317580054@qq.com>
2021-04-19 21:20:08 +0800

    v0.8.2 客户端IP地址, 端口号参数解析

--- client.go
+++ client.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
 )
@@ -31,8 +32,20 @@ func NewClient(serverIp string, serverPort int) *Client {
 	return client
 }
 
+var serverIp string
+var serverPort int
+
+func init() {
+	flag.StringVar(&serverIp, "ip", "127.0.0.1", "设置服务器IP地址(默认127.0.0.1)")
+	flag.IntVar(&serverPort, "port", 8888, "设置服务器端口号(默认8888)")
+}
+
 func main() {
-	client := NewClient("127.0.0.1", 8888)
+
+	// 解析命令行
+	flag.Parse()
+
+	client := NewClient(serverIp, serverPort)
 	if client == nil {
 		fmt.Println(">>>>> 连接服务器失败...")
 		return
